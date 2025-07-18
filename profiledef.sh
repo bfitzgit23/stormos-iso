f23e42c3df651386e22d92c02b8388e1ac950667
ben <dfitz@me.com>
2025-07-17 23:14:01 -0400

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -35,6 +35,6 @@ file_permissions=(
   ["/usr/local/bin/stormos-final"]="0:0:755"
   ["/usr/bin/calamares"]="0:0:755"
   ["/usr/bin/wifi-connection.sh"]="0:0:755"
-  ["/usr/bin/sbrowserv12.3.py"]="0:0:755"
+  ["/usr/bin/sbrowserv12.5.py"]="0:0:755"
 )
 
