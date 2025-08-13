888950e5ff26a21cb09088a33c9e4796b33d9d6a
ben <dfitz@me.com>
2025-08-13 12:57:46 -0400

    Update postinstall.sh

--- airootfs/usr/local/bin/postinstall.sh
+++ airootfs/usr/local/bin/postinstall.sh
@@ -83,8 +83,9 @@ echo "Defaults pwfeedback" | sudo EDITOR='tee -a' visudo >/dev/null 2>&1
 #pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 #pacman-key --lsign-key 3056513887B78AEB
 
-rm -rf /usr/share/backgrounds/xfce
 chown -R $name:$name /usr/share/backgrounds/* 
+rm -rf /usr/share/backgrounds/xfce/*
+cp -r /usr/share/backgrounds/* /usr/share/backgrounds/xfce/
 
 #rm $home/$name/Desktop/calamares.desktop
 
